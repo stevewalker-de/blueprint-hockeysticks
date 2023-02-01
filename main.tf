3bc42464d007ba0910a140e47f2645fc9b67b7f6
stevewalker-de <steveswalker@google.com>
2023-02-01 15:45:57 +0000

    v 1.3

--- main.tf
+++ main.tf
@@ -1,29 +1,89 @@
-module "bigquery" {
-  source                     = "../.."
-  dataset_id                 = "foo"
-  dataset_name               = "foo"
-  description                = "some description"
-  project_id                 = var.project_id
-  location                   = "US"
-  delete_contents_on_destroy = var.delete_contents_on_destroy
-  tables = [
-    {
-      table_id           = "bar",
-      schema             = file("sample_bq_schema.json"),
-      time_partitioning  = null,
-      range_partitioning = null,
-      expiration_time    = 2524604400000, # 2050/01/01
-      clustering         = [],
-      labels = {
-        env      = "devops"
-        billable = "true"
-        owner    = "joedoe"
-      },
-    }
+# --------------------------------------------------
+# TERRAFORM CONFIGURATION
+# Setting the provider and the project
+# --------------------------------------------------
+provider "google" {
+  project               = var.gcp_project
+  region                = var.gcp_region
+  zone                  = var.gcp_zone
+  user_project_override = true
+}
+
+data "google_project" "project" {
+}
+
+# --------------------------------------------------
+# RESOURCES
+# Note the comments below
+# --------------------------------------------------
+
+# Enable Data Transfer Service
+resource "google_project_service" "dts" {
+  project                    = data.google_project.project.project_id
+  service                    = "bigquerydatatransfer.googleapis.com"
+  disable_dependent_services = false
+  disable_on_destroy         = false
+}
+
+# Service Account
+resource "google_service_account" "bigquery_scheduled_queries" {
+  account_id   = "bigquery-scheduled-queries"
+  display_name = "BigQuery Scheduled Queries Service Account"
+  description  = "Used to run BigQuery Data Transfer jobs."
+}
+
+# Wait for the new Services and Service Accounts settings to propagate
+resource "time_sleep" "wait_for_settings_propagation" {
+  # It can take a while for the enabled services
+  # and service accounts to propagate. Experiment
+  # with this value until you find a time that is
+  # consistently working for all the deployments.
+  create_duration = "300s"
+
+  depends_on = [
+    google_project_service.dts,
+    google_service_account.bigquery_scheduled_queries
   ]
-  dataset_labels = {
-    env      = "dev"
-    billable = "true"
-    owner    = "janesmith"
+}
+
+# IAM Permisions
+resource "google_project_iam_member" "bigquery_scheduler_permissions" {
+  project = data.google_project.project.project_id
+  role   = "roles/iam.serviceAccountShortTermTokenMinter"
+  member = "serviceAccount:service-${data.google_project.project.number}@gcp-sa-bigquerydatatransfer.iam.gserviceaccount.com"
+
+  depends_on = [time_sleep.wait_for_settings_propagation]
+}
+
+resource "google_project_iam_binding" "bigquery_datatransfer_admin" {
+  project = data.google_project.project.project_id
+  role    = "roles/bigquery.admin"
+  members = ["serviceAccount:${google_service_account.bigquery_scheduled_queries.email}"]
+
+  depends_on = [time_sleep.wait_for_settings_propagation]
+}
+
+# Create the BigQuery dataset
+resource "google_bigquery_dataset" "my_dataset" {
+  depends_on = [google_project_iam_member.bigquery_scheduler_permissions]
+
+  dataset_id    = "my_dataset"
+  friendly_name = "My Dataset"
+  description   = "My Dataset with Scheduled Queries"
+  location      = var.gcp_region
+}
+
+resource "google_bigquery_data_transfer_config" "query_config" {
+  display_name           = "my-query"
+  location               = var.gcp_region
+  data_source_id         = "scheduled_query"
+  schedule               = "every day 00:00"
+  destination_dataset_id = google_bigquery_dataset.my_dataset.dataset_id
+  params = {
+    destination_table_name_template = "my_table"
+    write_disposition               = "WRITE_TRUNCATE"
+    query                           = "SELECT 1000 as total_users"
   }
+
+  depends_on = [google_project_iam_member.bigquery_scheduler_permissions]
 }
--- variables.tf
+++ variables.tf
@@ -1,25 +1,18 @@
-/**
- * Copyright 2021 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-variable "project_id" {
-  description = "Project where the dataset and table are created."
+# --------------------------------------------------
+# VARIABLES
+# Set these before applying the configuration
+# --------------------------------------------------
+variable gcp_project {
+  type        = string
+  description = "Google Cloud Project ID"
 }
 
-variable "delete_contents_on_destroy" {
-  description = "(Optional) If set to true, delete all the tables in the dataset when destroying the resource; otherwise, destroying the resource will fail if tables are present."
-  type        = bool
-  default     = null
+variable gcp_region {
+  type        = string
+  description = "Google Cloud Region"
+}
+
+variable gcp_zone {
+  type        = string
+  description = "Google Cloud Zone"
 }
