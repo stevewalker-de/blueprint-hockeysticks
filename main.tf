4b525dd224d55b251630e2e4a8d9926b1703e0fd
stevewalker-de <steveswalker@google.com>
2023-02-01 16:35:19 +0000

    v1.8

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
 provider "google" {
   project               = var.gcp_project
   region                = var.gcp_region
-  zone                  = var.gcp_zone
   user_project_override = true
 }
 
@@ -19,3 +18,14 @@ resource "google_bigquery_dataset" "A_1" {
   location      = var.gcp_region
 }
 
+resource "google_bigquery_table" "nyc" {
+  dataset_id = google_bigquery_dataset.A_1.dataset_id
+  table_id   = "nyc"
+
+  external_data_configuration {
+    autodetect    = true
+    source_format = "CSV"
+    source_uris = ["gs://steveswalker-morphic/NYC/new_york_taxi_trips-*.csv"]
+  }
+}
+#  zone                  = var.gcp_zone
