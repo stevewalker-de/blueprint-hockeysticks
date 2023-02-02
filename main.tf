9e16faad3a1b4254ae11778c9f6fc8077807f438
stevewalker-de <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>
2023-02-02 21:59:11 +0000

    thurs1

--- main.tf
+++ main.tf
@@ -8,17 +8,17 @@ data "google_project" "g-sql-morphic-luminous" {
 }
 
 # Create the BigQuery dataset
-resource "google_bigquery_dataset" "A_1" {
+resource "google_bigquery_dataset" "thurs_1" {
 
-  dataset_id    = "A_1"
-  friendly_name = "My A_1"
+  dataset_id    = "thurs_1"
+  friendly_name = "My thurs_1"
   description   = "My Dataset with Scheduled Queries"
   location      = var.region
 }
 
-resource "google_bigquery_table" "nyc" {
-  dataset_id = google_bigquery_dataset.A_1.dataset_id
-  table_id   = "nyc"
+resource "google_bigquery_table" "ATL1" {
+  dataset_id = google_bigquery_dataset.thurs_1.dataset_id
+  table_id   = "ATL1"
 
   external_data_configuration {
     autodetect    = true
@@ -26,10 +26,10 @@ resource "google_bigquery_table" "nyc" {
     source_uris = ["gs://steveswalker-morphic/NYC/new_york_taxi_trips-*.csv"]
   }
 }
-#comment
-resource "google_bigquery_table" "nyc2" {
-  dataset_id = google_bigquery_dataset.A_1.dataset_id
-  table_id   = "nyc2"
+
+resource "google_bigquery_table" "ATL2" {
+  dataset_id = google_bigquery_dataset.thurs_1.dataset_id
+  table_id   = "ATL2"
 
   external_data_configuration {
     autodetect    = true
@@ -37,3 +37,18 @@ resource "google_bigquery_table" "nyc2" {
     source_uris = ["gs://steveswalker-morphic/NYC/new_york_taxi_trips-*.csv"]
   }
 } 
+
+
+resource "google_notebooks_instance" "basic_instance" {
+  project      = var.project_id
+  name         = "edw-notebook-intro"
+  provider     = google
+  location     = "us-east1-b"
+  machine_type = "n1-standard-4"
+
+  vm_image {
+    project      = "deeplearning-platform-release"
+    image_family = "common-cpu-notebooks-debian-10"
+  }
+
+}
