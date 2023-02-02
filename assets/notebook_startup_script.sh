d3b92192d00eafc102b8640ff07960a84d218ee0
Steve W <67876607+stevewalker-de@users.noreply.github.com>
2023-02-02 16:03:46 -0500

    Add files via upload

--- assets/notebook_startup_script.sh
+++ assets/notebook_startup_script.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-wget 'https://github.com/stevewalker-de/blueprint-hockeysticks/blob/main/assets/Welcome%20to%20the%20EDW%20Solution%20walk-through!.ipynb'
+sudo wget 'https://raw.githubusercontent.com/stevewalker-de/blueprint-hockeysticks/main/assets/Welcome%20to%20the%20EDW%20Solution%20walk-through!.ipynb'
+sudo mv 'Welcome to the EDW Solution walk-through!.ipynb' /home/jupyter/edw.ipynb
