c95f0e38c2e4bd3da19284d4dcbefc1cbabb8b46
stevewalker-de <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>
2023-02-04 15:29:20 +0000

    02041

--- main.tf
+++ main.tf
@@ -8,40 +8,29 @@ data "google_project" "g-sql-morphic-luminous" {
 }
 
 # Create the BigQuery dataset
-resource "google_bigquery_dataset" "A_DS_1" {
+resource "google_bigquery_dataset" "ds_edw" {
 
-  dataset_id    = "A_DS_1"
-  friendly_name = "My A_DS_1"
-  description   = "My Dataset with Scheduled Queries"
+  dataset_id    = "ds_edw"
+  friendly_name = "My EDW Dataset"
+  description   = "My EDW Dataset with tables"
   location      = var.region
 }
 
-resource "google_bigquery_table" "tbl_DW1" {
-  dataset_id = google_bigquery_dataset.A_DS_1.dataset_id
-  table_id   = "tbl_DW1"
+resource "google_bigquery_table" "tbl_edw_taxi" {
+  dataset_id = google_bigquery_dataset.ds_edw.dataset_id
+  table_id   = "tbl_edw_taxi"
 
   external_data_configuration {
     autodetect    = true
     source_format = "CSV"
-    source_uris = ["gs://steveswalker-morphic/NYC/new_york_taxi_trips-*.csv"]
+    source_uris = ["gs://solution-data-taxi-trips/new_york_taxi_trips-*.csv"]
   }
 }
-
-resource "google_bigquery_table" "tbl_DW2" {
-  dataset_id = google_bigquery_dataset.A_DS_1.dataset_id
-  table_id   = "tbl_DW2"
-
-  external_data_configuration {
-    autodetect    = true
-    source_format = "CSV"
-    source_uris = ["gs://steveswalker-morphic/NYC/new_york_taxi_trips-*.csv"]
-  }
-} 
 
 
 resource "google_notebooks_instance" "basic_instance" {
   project                = var.project_id
-  name                   = "edw-notebook-intro-airmj-3"
+  name                   = "edw-notebook-intro-0204v1"
   provider               = google
   location               = "us-east1-b"
   machine_type           = "n1-standard-4"
@@ -57,4 +46,38 @@ resource "google_notebooks_instance" "basic_instance" {
 }
 
 
+resource "google_cloudfunctions2_function" "function" {
+  #provider = google-beta
+  project     = var.project_id
+  name        = "gcs-load-bq"
+  location    = "us-central1"
+  description = "gcs-load-bq"
 
+  build_config {
+    runtime     = "python310"
+    entry_point = "gcs_load_bq"
+    source {
+      storage_source {
+        bucket = "solution-cdc-assets-sandbox"
+        object = "function.zip"
+      }
+    }
+  }
+
+  service_config {
+    max_instance_count = 1
+    available_memory   = "256M"
+    timeout_seconds    = 60
+  }
+
+  event_trigger {
+    trigger_region = "us-central1"
+    event_type     = "google.cloud.storage.object.v1.finalized"
+    event_filters {
+         attribute = "bucket"
+         value = "solution-cdc-bucket-sandbox"
+    }
+    retry_policy   = "RETRY_POLICY_RETRY"
+  }
+
+} 
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,8 @@
 output "ds_friendly_name" {
-  value = google_bigquery_dataset.A_DS_1.friendly_name
+  value = "blah" #google_bigquery_dataset.ds_edw.friendly_name
 }
+
+output "function_uri" {
+  value = google_cloudfunctions2_function.function.service_config[0].uri
+}
+
