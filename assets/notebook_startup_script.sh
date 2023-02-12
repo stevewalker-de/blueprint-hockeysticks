7fd341866931ce4a64abba49deb7b81e388ef898
davenportjw <jasondavenport@google.com>
2023-02-11 17:37:34 -0700

    remove scratch notebooks (#4)

--- assets/notebook_startup_script.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sudo wget 'https://raw.githubusercontent.com/stevewalker-de/blueprint-hockeysticks/main/assets/Welcome%20to%20the%20EDW%20Solution%20walk-through!.ipynb'
-sudo mv 'Welcome to the EDW Solution walk-through!.ipynb' /home/jupyter/edw.ipynb
